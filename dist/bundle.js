09283b21aae45e84ff51bfca7fab931bf9ec4fd6
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-04-15 16:29:40 -0300

    Configurando Babel

---
+++ dist/bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.App = App;
+
+function App() {
+  return /*#__PURE__*/React.createElement("h1", null, "Hello world");
+}
