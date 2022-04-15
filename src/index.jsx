f8ecaa6f132ea18cad6da16d782f4aad7e64bc18
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-04-15 17:25:35 -0300

    Estrutura do ReactJS

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,6 @@
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root')
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
 import { App } from './App'
+
+root.render(<App />)
